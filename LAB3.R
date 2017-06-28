c28ddf9ecbed6d73057a519250d87fda2c36ca0c
uday singh <f71uday@gmail.com>
2017-06-29 02:24:09 +0530

    --Q5

--- LAB3.R
+++ LAB3.R
@@ -9,3 +9,16 @@ x<-data.frame(matrix(rnorm(k*r)),nrow=r))
 my.function<-function(x){
   return(c(min(x),median(x),mean(x),max(x)))
 }
+#4
+sapply(x,my.function)
+#5
+
+time1<-as.numeric(Sys.time())
+for(i in 1:k2){
+  mean1[i]<-mean(my.data[,i])
+}
+time2<-as.numeric(Sys.time())
+time3<-as.numeric(Sys.time())
+mean2<-sapply(my.data,mean)
+time4<-as.numeric(Sys.time())
+(time2-time1)/(time4-time3)
