c6c5287bfe80dc2e8bbef120ba1057cf96d4c5c7
uday singh <f71uday@gmail.com>
2017-06-29 01:35:03 +0530

    time fator of for WRT sapply()

--- EX3_3.R
+++ EX3_3.R
@@ -6,3 +6,13 @@ for(i in 1:k2){
 
 }
 cat(mean2<-sapply(my.data,mean))
+
+time1<-as.numeric(Sys.time())
+for(i in 1:k2){
+  mean1[i]<-mean(my.data[,i])
+}
+time2<-as.numeric(Sys.time())
+time3<-as.numeric(Sys.time())
+mean2<-sapply(my.data,mean)
+time4<-as.numeric(Sys.time())
+(time2-time1)/(time4-time3)
